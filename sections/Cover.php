aafb51a4169827e86dc8c3ef9731aba795f86a4a
Adem ilter <ademilter@gmail.com>
2016-10-19 00:34:52 +0300

    photos format set webp

--- sections/Cover.php
+++ sections/Cover.php
@@ -2,12 +2,12 @@
   <div class="container-xl">
     <picture>
       <source media="(min-width: 1200px)"
-              srcset="http://adem.imgix.net/site-cover-photo.jpg?w=1.0&h=1300&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.3&auto=format&q=70">
+              srcset="http://adem.imgix.net/site-cover-photo.jpg?w=1.0&h=1300&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.3&auto=format&q=60">
       <source media="(min-width: 992px)"
-              srcset="http://adem.imgix.net/site-cover-photo.jpg?w=1200&h=900&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.3&auto=format&q=70">
+              srcset="http://adem.imgix.net/site-cover-photo.jpg?w=1200&h=900&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.3&auto=format&q=60">
       <source media="(min-width: 768px)"
-              srcset="http://adem.imgix.net/site-cover-photo.jpg?w=992&h=800&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.3&auto=format&q=70">
-      <img srcset="http://adem.imgix.net/site-cover-photo.jpg?w=768&auto=format&q=70"
+              srcset="http://adem.imgix.net/site-cover-photo.jpg?w=992&h=800&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.3&auto=format&q=60">
+      <img srcset="http://adem.imgix.net/site-cover-photo.jpg?w=768&auto=format&q=60"
            alt="Adem ilter with daughter">
     </picture>
   </div>
